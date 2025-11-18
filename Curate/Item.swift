c40755b8644bc6e54afbd62afae8e8c4330729e2
kevintchou <kevintjchou@gmail.com>
2025-11-18 11:29:51 -0600

    Initial Commit

---
+++ Curate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Curate
+//
+//  Created by Kevin Chou on 11/18/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CurateTests/CurateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurateTests.swift
+//  CurateTests
+//
+//  Created by Kevin Chou on 11/18/25.
+//
+
+import Testing
+@testable import Curate
+
+struct CurateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
